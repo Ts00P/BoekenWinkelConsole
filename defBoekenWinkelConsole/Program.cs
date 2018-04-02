923294797321ffd4d2209170a49b6b106373665e
Teun <teun-steggink@hotmail.nl>
2018-04-02 23:11:58 +0200

    Format changes

--- defBoekenWinkelConsole/Program.cs
+++ defBoekenWinkelConsole/Program.cs
@@ -11,10 +11,9 @@ namespace defBoekenWinkelConsole
     {
         public static void Main(string[] args)
         {
-            BoekenWinkel.BoekenWinkel bo = AddData.GetData();
+            var boekenWinkel = AddData.GetData();
 
-
-            for (int i = 0; i < 100000; i++)
+            for (var i = 0; i < 100000; i++)
             {
 
                 Console.WriteLine("[1] Laatste bestelling afdrukken");
@@ -32,7 +31,7 @@ namespace defBoekenWinkelConsole
                 Console.WriteLine("[R] Voeg een nieuw tijdschrift toe");
                 Console.WriteLine("[T] Bestellingen afhandelen");
 
-                ConsoleKeyInfo keyinfo = Console.ReadKey();
+                var keyinfo = Console.ReadKey();
                 Console.Clear();
 
                 switch (keyinfo.Key)
@@ -40,17 +39,17 @@ namespace defBoekenWinkelConsole
 
 
                     case ConsoleKey.D1:
-                        Console.Write(bo.Bestellingen.LaasteBestellingAfdrukken());
+                        Console.Write(boekenWinkel.Bestellingen.LaasteBestellingAfdrukken());
                         break;
 
                     case ConsoleKey.D2:
                         try
                         {
                             Console.WriteLine("Geef het ISSN nummer op:");
-                            string id = Console.ReadLine();
+                            var id = Console.ReadLine();
                             Console.WriteLine("Geef het aantal verkochte tijdschriften op:");
                             int aantalverkocht = Convert.ToInt16(Console.ReadLine());
-                            Console.Write(bo.VerkoopTijdschrift(id, aantalverkocht));
+                            Console.Write(boekenWinkel.VerkoopTijdschrift(id, aantalverkocht));
                         }
                         catch
                         {
@@ -66,72 +65,70 @@ namespace defBoekenWinkelConsole
                             Console.WriteLine("[1] Via IBBN");
                             Console.WriteLine("[2] Via titel en auteur");
 
-                            ConsoleKeyInfo key = Console.ReadKey();
+                            var key = Console.ReadKey();
                             if (key.Key == ConsoleKey.D1)
                             {
 
                                 Console.WriteLine("Geef het IBBN nummer op:");
-                                string ibbn = Console.ReadLine();
+                                var ibbn = Console.ReadLine();
                                 Console.WriteLine("Geef het aantal verkochte boeken op:");
                                 int aantal = Convert.ToInt16(Console.ReadLine());
-                                Console.Write(bo.VerkoopBoek(ibbn, aantal));
+                                Console.Write(boekenWinkel.VerkoopBoek(ibbn, aantal));
 
                             }
 
                             if (key.Key == ConsoleKey.D2)
                             {
                                 Console.WriteLine("Geef de titel op:");
-                                string tiel = Console.ReadLine();
+                                var tiel = Console.ReadLine();
                                 Console.WriteLine("Geef de auteur op:");
-                                string auteur = Console.ReadLine();
+                                var auteur = Console.ReadLine();
                                 Console.WriteLine("Geef het aantal verkochte boeken op:");
                                 int aantalverkochts = Convert.ToInt16(Console.ReadLine());
-                                Console.Write(bo.VerkoopBoek(tiel, auteur, aantalverkochts));
+                                Console.Write(boekenWinkel.VerkoopBoek(tiel, auteur, aantalverkochts));
 
                             }
                         }
                         catch
                         {
-
                             Console.WriteLine("Geen geldige waardes ingevoerd.");
-
                         }
 
                         break;
 
                     case ConsoleKey.D4:
 
-                        Console.Write(bo.ToonBoekVoorraad());
+                        Console.Write(boekenWinkel.ToonBoekVoorraad());
                         break;
 
                     case ConsoleKey.D5:
-                        Console.Write(bo.ToonTijdschriftVoorraad());
+                        Console.Write(boekenWinkel.ToonTijdschriftVoorraad());
                         break;
 
                     case ConsoleKey.D6:
                         Console.WriteLine("Geef het id op:");
-                        bo.VerwijderProduct(Console.ReadLine());
+                        boekenWinkel.VerwijderProduct(Console.ReadLine());
                         Console.WriteLine("Het product is verwijderd.");
                         break;
 
                     case ConsoleKey.D7:
-                        Console.Write(bo.NietVerwerkteBestellingen());
+                        Console.Write(boekenWinkel.NietVerwerkteBestellingen());
                         break;
 
                     case ConsoleKey.D8:
-                        Console.Write(bo.GenereerBestelLijst());
+                        Console.Write(boekenWinkel.GenereerBestelLijst());
                         break;
 
                     case ConsoleKey.D9:
                         try
                         {
                             Console.WriteLine("Geef het IBBN nummer op:");
-                            Boek boek = (Boek)bo.ZoekProduct(Console.ReadLine());
+                            Boek boek = (Boek)boekenWinkel.ZoekProduct(Console.ReadLine());
                             Console.WriteLine("Geef het nieuwe min aan:");
                             int min = Convert.ToInt16(Console.ReadLine());
                             Console.WriteLine("Geef het nieuwe max aan:");
                             int max = Convert.ToInt16(Console.ReadLine());
-                            bo.BoekVoorraadAanpassen(boek, min, max);
+                            boekenWinkel.BoekVoorraadAanpassen(boek, min, max);
                             Console.WriteLine("De aanpassing is correct voltooid!");
                             break;
                         }
@@ -146,10 +143,10 @@ namespace defBoekenWinkelConsole
                         try
                         {
                             Console.WriteLine("Geef het ISSN nummer op:");
-                            Tijdschrift tijd = (Tijdschrift)bo.ZoekProduct(Console.ReadLine());
+                            var tijd = (Tijdschrift)boekenWinkel.ZoekProduct(Console.ReadLine());
                             Console.WriteLine("Geef het nieuwe aantal op:");
                             int nieuwsaant = Convert.ToInt16(Console.ReadLine());
-                            bo.TijdschriftBestelAantalAanpassen(tijd, nieuwsaant);
+                            boekenWinkel.TijdschriftBestelAantalAanpassen(tijd, nieuwsaant);
 
                             Console.WriteLine("Het aantal is aangepast!");
                         }
@@ -165,8 +162,8 @@ namespace defBoekenWinkelConsole
                         try
                         {
                             Console.WriteLine("Geef een zoek datum op:");
-                            DateTime date = Convert.ToDateTime(Console.ReadLine());
-                            Console.Write(bo.Bestellingen.BestellingAfdrukkenOpDatum(date));
+                            var date = Convert.ToDateTime(Console.ReadLine());
+                            Console.Write(boekenWinkel.Bestellingen.BestellingAfdrukkenOpDatum(date));
                         }
                         catch
                         {
@@ -178,17 +175,17 @@ namespace defBoekenWinkelConsole
                         try
                         {
                             Console.WriteLine("Geef de titel op:");
-                            string tile = Console.ReadLine();
+                            var tile = Console.ReadLine();
                             Console.WriteLine("Geef de auteur op:");
-                            string auteur = Console.ReadLine();
+                            var auteur = Console.ReadLine();
                             Console.WriteLine("Geef de ISBN op:");
-                            string ibbn = Console.ReadLine();
+                            var ibbn = Console.ReadLine();
                             Console.WriteLine("Geef de druk op:");
-                            string druk = Console.ReadLine();
+                            var druk = Console.ReadLine();
                             Console.WriteLine("Geef het gewicht op:");
                             int gewicht = Convert.ToInt16(Console.ReadLine());
                             Console.WriteLine("Geef de prijs op:");
-                            decimal prijs = Convert.ToDecimal(Console.ReadLine());
+                            var prijs = Convert.ToDecimal(Console.ReadLine());
                             Console.WriteLine("Geef de voorraad op:");
                             int voorraad = Convert.ToInt16(Console.ReadLine());
                             Console.WriteLine("Geef het max voorraad op:");
@@ -196,12 +193,12 @@ namespace defBoekenWinkelConsole
                             Console.WriteLine("Geef het min voorraad op:");
                             int minvoorraad = Convert.ToInt16(Console.ReadLine());
                             Console.WriteLine("Geef de lengte op:");
-                            double lengte = Convert.ToDouble(Console.ReadLine());
+                            var lengte = Convert.ToDouble(Console.ReadLine());
                             Console.WriteLine("Geef de breedte op:");
-                            double breedte = Convert.ToDouble(Console.ReadLine());
+                            var breedte = Convert.ToDouble(Console.ReadLine());
                             Console.WriteLine("Geef de hoogte op:");
-                            double hoogte = Convert.ToDouble(Console.ReadLine());
-                            bo.NieuwBoek(ibbn, druk, tile, auteur, gewicht, prijs, voorraad, maxvoorraad, minvoorraad, hoogte, breedte, lengte);
+                            var hoogte = Convert.ToDouble(Console.ReadLine());
+                            boekenWinkel.NieuwBoek(ibbn, druk, tile, auteur, gewicht, prijs, voorraad, maxvoorraad, minvoorraad, hoogte, breedte, lengte);
                             Console.WriteLine("Boek is correct toegevoegd!");
                         }
                         catch
@@ -214,23 +211,23 @@ namespace defBoekenWinkelConsole
 
                         try
                         {
-                            DayOfWeek besteldag = DayOfWeek.Friday;
-                            DayOfWeek publicatiedayg = DayOfWeek.Friday;
+                            var besteldag = DayOfWeek.Friday;
+                            var publicatiedayg = DayOfWeek.Friday;
 
                             Console.WriteLine("Geef het ISSN nummer op");
-                            string issb1 = Console.ReadLine();
+                            var issb1 = Console.ReadLine();
                             Console.WriteLine("Geef de titel op:");
-                            string tiel1 = Console.ReadLine();
+                            var tiel1 = Console.ReadLine();
                             Console.WriteLine("Geef de auteur op:");
-                            string auteur1 = Console.ReadLine();
+                            var auteur1 = Console.ReadLine();
                             Console.WriteLine("Geef de hoogte op:");
-                            double hoogte1 = Convert.ToDouble(Console.ReadLine());
+                            var hoogte1 = Convert.ToDouble(Console.ReadLine());
                             Console.WriteLine("Geef de breedte op:");
-                            double breedte1 = Convert.ToDouble(Console.ReadLine());
+                            var breedte1 = Convert.ToDouble(Console.ReadLine());
                             Console.WriteLine("Geef de lengte op:");
-                            double lengte1 = Convert.ToDouble(Console.ReadLine());
+                            var lengte1 = Convert.ToDouble(Console.ReadLine());
                             Console.WriteLine("Geef de prijs op:");
-                            decimal prijs1 = Convert.ToDecimal(Console.ReadLine());
+                            var prijs1 = Convert.ToDecimal(Console.ReadLine());
                             Console.WriteLine("Geef het gewicht op:");
                             int gewicht1 = Convert.ToInt16(Console.ReadLine());
                             Console.WriteLine("Geef het aantal te bestellen tijdschriften op:");
@@ -246,7 +243,7 @@ namespace defBoekenWinkelConsole
                             Console.WriteLine("[6] Zaterdag");
                             Console.WriteLine("[7] Zondag");
 
-                            ConsoleKeyInfo ainfo = Console.ReadKey();
+                            var ainfo = Console.ReadKey();
 
                             if (ainfo.Key == ConsoleKey.D1)
                             {
@@ -286,7 +283,7 @@ namespace defBoekenWinkelConsole
                             Console.WriteLine("[6] Zaterdag");
                             Console.WriteLine("[7] Zondag");
 
-                            ConsoleKeyInfo ainfo1 = Console.ReadKey();
+                            var ainfo1 = Console.ReadKey();
 
                             if (ainfo1.Key == ConsoleKey.D1)
                             {
@@ -317,7 +314,7 @@ namespace defBoekenWinkelConsole
                                 publicatiedayg = DayOfWeek.Sunday;
                             }
 
-                            bo.NieuwTijdschrift(tiel1, auteur1, hoogte1, breedte1, lengte1, gewicht1, prijs1, issb1, aantaltijd1, besteldag, publicatiedayg, voorraad1);
+                            boekenWinkel.NieuwTijdschrift(tiel1, auteur1, hoogte1, breedte1, lengte1, gewicht1, prijs1, issb1, aantaltijd1, besteldag, publicatiedayg, voorraad1);
                             Console.WriteLine("Tijdschrift toegevoegd!");
                         }
                         catch
@@ -326,12 +323,10 @@ namespace defBoekenWinkelConsole
                         }
                         break;
 
-
                     case ConsoleKey.T:
-                        Console.WriteLine(bo.BestellingAfgehandeld());
+                        Console.WriteLine(boekenWinkel.BestellingAfgehandeld());
                         break;
                 }
-
                 Console.ReadKey();
                 Console.Clear();
             }
